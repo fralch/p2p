d721c04d730bb04d0aeadd87590e34a7802d19d7
Frank Cairampoma <ingfralch@gmail.com>
2023-01-05 17:37:08 -0500

    tratando de crear las funciones nodo 1

--- nodo1.js
+++ nodo1.js
@@ -2,8 +2,6 @@ import process from 'node:process'
 import { createLibp2p } from 'libp2p'
 import { tcp } from '@libp2p/tcp'
 import { noise } from '@chainsafe/libp2p-noise'
-import { mplex } from '@libp2p/mplex'
-import { multiaddr } from 'multiaddr'
 
 const node = await createLibp2p({
   addresses: {
@@ -15,32 +13,8 @@ const node = await createLibp2p({
   streamMuxers: [mplex()]
 })
 
-// start libp2p
-await node.start()
-console.log('libp2p has started')
+// enviar mensaje a nodo2
+const { stream } = await node.dialProtocol()
 
-// print out listening addresses
-console.log('listening on addresses:')
-node.getMultiaddrs().forEach((addr) => {
-  console.log(addr.toString())
-})
 
-// ping peer if received multiaddr
-if (process.argv.length >= 3) {
-  const ma = multiaddr(process.argv[2])
-  console.log(`pinging remote peer at ${process.argv[2]}`)
-  const latency = await node.ping(ma)
-  console.log(`pinged ${process.argv[2]} in ${latency}ms`)
-} else {
-  console.log('no remote peer address given, skipping ping')
-}
 
-const stop = async () => {
-  // stop libp2p
-  await node.stop()
-  console.log('libp2p has stopped')
-  process.exit(0)
-}
-
-process.on('SIGTERM', stop)
-process.on('SIGINT', stop)
